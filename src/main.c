1ec5cfd0013d9cb29ba42808fcafa92403106eeb
zhigunovsemyon <zhigunovsemen@gmail.com>
2024-09-23 11:55:43 +0300

    Сделал вывод справки при неправильном использовании

--- src/main.c
+++ src/main.c
@@ -1,11 +1,23 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 /*В файле записан текст стихотворения. Форматировать текст так,
 чтобы каждый куплет из 4 строк следовал с одной и той же позиции,
 начинался с прописной буквы и был сдвинут относительно предыдущего
 куплета на 5 позиций вправо или влево поочередно. */
 
-int main (void) {
+//Вывод справки об использовании
+void usage(char const * const filename) {
+	printf("%s ПУТЬ_К_ФАЙЛУ\n", filename);
+}
+
+int main (int const argc, char const * const * const args) {
+	//Вывод справки об использовании при неправильном вводе
+	if (argc != 2) {
+		usage(args[0]);
+		return EXIT_SUCCESS;
+	}
+
     puts("Hello world!");
     return EXIT_SUCCESS;
 }

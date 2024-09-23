9b5306567a02517e6dde40cf9f777ba7b10b9133
Semyon <zhigunovsemen@gmail.com>
2024-09-23 17:35:12 +0300

    Победил clang-format

--- src/main.c
+++ src/main.c
@@ -1,17 +1,15 @@
+#include <stdbool.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
-#include <stdbool.h>
 /*В файле записан текст стихотворения. Форматировать текст так,
 чтобы каждый куплет из 4 строк следовал с одной и той же позиции,
 начинался с прописной буквы и был сдвинут относительно предыдущего
 куплета на 5 позиций вправо или влево поочередно. */
 
-inline void PutIdent(void) {
-	printf("     ");
-}
+inline void PutIdent(void) { printf("     "); }
 
-//Вычисление длинны файла fptr
+// Вычисление длинны файла fptr
 long FileLen(FILE *const fptr) {
     fseek(fptr, SEEK_SET, SEEK_END);
     long const len = ftell(fptr);
@@ -19,43 +17,43 @@ long FileLen(FILE *const fptr) {
     return len;
 }
 
-//Вывод справки об использовании
-void usage(char const * const filename) {
-	printf("%s ПУТЬ_К_ФАЙЛУ\n", filename);
+// Вывод справки об использовании
+void usage(char const *const filename) {
+    printf("%s ПУТЬ_К_ФАЙЛУ\n", filename);
 }
 
-int main (int const argc, char const * const * const args) {
-	//Вывод справки об использовании при неправильном вводе
-	if (argc != 2) {
-		usage(args[0]);
-		return EXIT_SUCCESS;
-	}
+int main(int const argc, char const *const *const args) {
+    // Вывод справки об использовании при неправильном вводе
+    if (argc != 2) {
+        usage(args[0]);
+        return EXIT_SUCCESS;
+    }
 
-	//Открываемый файл с текстом
-	FILE *src;
-	//Открытие файла, вывод ошибки при неудаче
-	if (NULL == (src = fopen(args[1], "rt"))){
-		perror(args[1]);
-		return EXIT_FAILURE;
-	}
+    // Открываемый файл с текстом
+    FILE *src;
+    // Открытие файла, вывод ошибки при неудаче
+    if (NULL == (src = fopen(args[1], "rt"))) {
+        perror(args[1]);
+        return EXIT_FAILURE;
+    }
 
-	// bool Ident = true;
-	[[maybe_unused]]size_t line = 0;
-	// bool FirstChar = true;
-	while (true){
-		int curChar = fgetc(src);
-		switch (curChar) {
-			case '\r':
-			break;
-			case EOF:
-			fclose(src); //Закрытие файла
-		    return EXIT_SUCCESS;
-			break;
-			case '\n':
-			line++;
-			default:
-			putchar(curChar);
-			break;
-		}
-	}
+    // bool Ident = true;
+    [[maybe_unused]] size_t line = 0;
+    // bool FirstChar = true;
+    while (true) {
+        int curChar = fgetc(src);
+        switch (curChar) {
+        case '\r':
+            break;
+        case EOF:
+            fclose(src); // Закрытие файла
+            return EXIT_SUCCESS;
+            break;
+        case '\n':
+            line++;
+        default:
+            putchar(curChar);
+            break;
+        }
+    }
 }
